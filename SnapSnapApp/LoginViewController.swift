7ae9b49d8fd75e84023a8a50ad5e6d299ef6ff6f
Anton Veldanov <anton.veldanov@gmail.com>
2020-05-11 14:17:01 -0700

    iOS

---
+++ SnapSnapApp/LoginViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginViewController.swift
+//  SnapSnapApp
+//
+//  Created by Veldanov, Anton on 5/11/20.
+//  Copyright © 2020 Anton Veldanov. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+  
+  
+  @IBOutlet weak var emailTextField: UITextField!
+  @IBOutlet weak var passwordTextField: UITextField!
+  @IBOutlet weak var topButtonOutlet: UIButton!
+  @IBOutlet weak var bottomButtonOutlet: UIButton!
+  
+  
+  
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
